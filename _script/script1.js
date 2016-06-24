6e6120bb938ff02c3941b767b159b551d62609a2
Edilson <edilsonfreire2008@hotmail.com>
2016-06-23 23:17:44 -0300

    Commit Inicial

---
+++ _script/script1.js
@@ -0,0 +1,7 @@
+function lampadaLigada(){
+    document.getElementById('luz').src = "_image/lampada-acesa.jpg";
+}
+
+function lampadaApagada(){
+    document.getElementById('luz').src = "_image/lampada-apagada.jpg";
+}
